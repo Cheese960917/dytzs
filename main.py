87481be062fe9a0d968b5c627d8a69829730a802
Ritchie <renqi189@icloud.com>
2022-07-09 21:44:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import time
 import datetime
 
 import requests
@@ -36,7 +37,7 @@ import csv
 
 # data source from daoshuju.com (unauthorized)
 req_url = 'https://daoshuju.com/api/project/record'
-player_name = 'yyfyyf'  # 玩家ID，注意这里的ID需要和网站上面的ID相同
+player_name = '小刘666'  # 玩家ID，注意这里的ID需要和网站上面的ID相同
 code_cup = 'dy220701'  # 第一届天之上杯: dy220509，第二届 dy220701
 size = 500  # 获取多少条比赛记录
 item_map = {}  # 对照表
@@ -355,11 +356,82 @@ def all_titles_statistic():
 
 
 # 僵后选人胜率
-# def pick_player_statistic():
-#     def sort_by_time(record):
-#         return record.time
-#     new_list = all_records.copy()
-#
+def pick_player_statistic():
+    def sort_by_time(record):
+        return time.mktime(datetime.datetime.strptime(
+            record.time, "%Y-%m-%d %H:%M:%S").timetuple())
+
+    new_list = all_records.copy()  # 由于要排序，copy一下
+    new_list.sort(key=sort_by_time) # 按时间排序
+    win = 0
+    lose = 0
+    flag = 0  # 本局拿到僵的标志
+    jdate = None  # 拿到僵的日期
+    pick_record_list = []  # 僵选人的场合
+    for record in new_list:
+        title_list = record.title_list
+        if flag == 1:
+            # 这里代表上一把拿到了僵
+            flag = 0
+            if jdate != datetime.datetime.strptime(record.time, "%Y-%m-%d %H:%M:%S").date():
+                # 如果僵过后那一局没有发生在同一天
+                jdate = None
+                continue
+            else:
+                # 僵选人的那一局
+                jdate = None
+                pick_record_list.append(record)
+                if record.result == '胜利':
+                    win += 1
+                else:
+                    lose += 1
+        if title_list is None or len(title_list) == 0:
+            continue
+        for title in title_list:
+            # 这里判断这一把有没有拿到僵
+            if title == '僵':
+                flag = 1
+                jdate = datetime.datetime.strptime(record.time, "%Y-%m-%d %H:%M:%S").date()
+    total = win + lose
+    rate = win / total
+    print(f'{player_name} 在拿到僵后一共选了 {total} 次人, 其中胜利 {win} 次， 失败 {lose} 次，选人胜率为 {round(rate, 2)}')
+    print('注意存在以下情况：')
+    print('1、选手拿到僵时在当天最后一局，之后不打了')
+    print('2、选手拿到僵后去吃饭了，跳过了后面几局')
+    print('这些情况难以统计，所以概率可能出现误差，敬请谅解。')
+    picked_alies_statistic(pick_record_list)
+
+
+# 僵选人的队友胜率
+def picked_alies_statistic(picked_list):
+    allies_map = {}
+    for record in picked_list:
+        for ally in record.alies:
+            player = ally.playr
+            win = record.result == '胜利'
+            if player not in allies_map:
+                allies_map[player] = []
+            if win:
+                allies_map[player].append(1)
+            else:
+                allies_map[player].append(0)
+    hero_value = []
+    for ally in allies_map:
+        values = allies_map[ally]
+        win = 0
+        lose = 0
+        sum_of_ally = 0
+        for i in values:
+            sum_of_ally += i
+            if i == 1:
+                win += 1
+            else:
+                lose += 1
+        avg = sum_of_ally / len(values)
+        hero_value.append([ally, win, lose, round(avg, 2)])
+    header = ['选人', '胜场', '负场', '胜率']
+    file_name = datetime.date.today().__str__() + '_' + player_name + '_picked_ally' + '.csv'
+    write_into_file(file_name, header, hero_value)
 
 # 把内容写入文件
 # file_name: 文件名
@@ -383,5 +455,6 @@ if __name__ == '__main__':
     all_ult_statistics()
     all_skill_statistics()
     all_titles_statistic()
+    pick_player_statistic()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
